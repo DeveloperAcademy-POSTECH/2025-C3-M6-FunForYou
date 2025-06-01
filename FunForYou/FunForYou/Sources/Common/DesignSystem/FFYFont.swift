28d0b2b5f1964944b2bf2bd8392bbbef22627b8c
pearhyunjin <pearhyunjin@gmail.com>
2025-06-01 11:02:42 +0900

    🍱 에셋 부분 변경

--- FunForYou/FunForYou/Sources/Common/DesignSystem/FFYFont.swift
+++ FunForYou/FunForYou/Sources/Common/DesignSystem/FFYFont.swift
@@ -9,31 +9,31 @@ import SwiftUI
 
 enum FFYFont {
     /// font: Pretendard, size: 34, weight: bold
-    static let LargeTitle: Font = .custom("Pretendard-Bold", size: 34)
+    static let largeTitle: Font = .custom("Pretendard-Bold", size: 34)
     /// font: Pretendard, size: 28, weight: bold
-    static let Title1: Font = .custom("Pretendard-Bold", size: 28)
+    static let title1: Font = .custom("Pretendard-Bold", size: 28)
     /// font: Pretendard, size: 22, weight: bold
-    static let Title2: Font = .custom("Pretendard-Bold", size: 22)
+    static let title2: Font = .custom("Pretendard-Bold", size: 22)
     /// font: Pretendard, size: 20, weight: bold
-    static let Title3: Font = .custom("Pretendard-Bold", size: 20)
+    static let title3: Font = .custom("Pretendard-Bold", size: 20)
     /// font: Pretendard, size: 17, weight: semibold
-    static let Headline: Font = .custom("Pretendard-SemiBold", size: 17)
+    static let headline: Font = .custom("Pretendard-SemiBold", size: 17)
     /// font: Pretendard, size: 17, weight: regular
-    static let Body: Font = .custom("Pretendard-Regular", size: 17)
+    static let body: Font = .custom("Pretendard-Regular", size: 17)
     /// font: Pretendard, size: 16, weight: regular
-    static let Callout: Font = .custom("Pretendard-Regular", size: 16)
+    static let callout: Font = .custom("Pretendard-Regular", size: 16)
     /// font: Pretendard, size: 15, weight: regular
-    static let SubHeadline: Font = .custom("Pretendard-Regular", size: 15)
+    static let subHeadline: Font = .custom("Pretendard-Regular", size: 15)
     /// font: Pretendard, size: 13, weight: regular
-    static let Footnote: Font = .custom("Pretendard-Regular", size: 13)
+    static let footnote: Font = .custom("Pretendard-Regular", size: 13)
     /// font: Pretendard, size: 12, weight: regular
-    static let Caption1: Font = .custom("Pretendard-Regular", size: 12)
+    static let caption1: Font = .custom("Pretendard-Regular", size: 12)
     /// font: Pretendard, size: 11, weight: regular
-    static let Caption2: Font = .custom("Pretendard-Regular", size: 11)
+    static let caption2: Font = .custom("Pretendard-Regular", size: 11)
     /// font: Kimjungchul, size: 18, weight: regular
-    static let TitleK: Font = .custom("KimjungchulMyungjo-Regular", size: 18)
+    static let book: Font = .custom("KimjungchulMyungjo-Regular", size: 18)
     /// font: Kimjungchul, size: 16, weight: regular
-    static let HeadlineK: Font = .custom("KimjungchulMyungjo-Regular", size: 16)
+    static let page: Font = .custom("KimjungchulMyungjo-Regular", size: 16)
     /// font: Kimjungchul, size: 14, weight: regular
-    static let BodyK: Font = .custom("KimjungchulMyungjo-Regular", size: 14)
+    static let pages: Font = .custom("KimjungchulMyungjo-Regular", size: 14)
 }
