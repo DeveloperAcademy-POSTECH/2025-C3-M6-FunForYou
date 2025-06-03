1bbf2670aa6500f37e3cb9a7e7bb17bf79b441a2
pearhyunjin <pearhyunjin@gmail.com>
2025-06-04 02:42:33 +0900

    ✨ [feat] #36 viewModel 사용해 state로 상태 변수 관리하기

--- FunForYou/FunForYou/Sources/Presentation/Daily/DailyWriting/DailyWritingView.swift
+++ FunForYou/FunForYou/Sources/Presentation/Daily/DailyWriting/DailyWritingView.swift
@@ -9,15 +9,6 @@ import SwiftUI
 
 struct DailyWritingView: View {
     @StateObject var viewModel: DailyWritingViewModel
-    @State private var dailyTitle: String = ""
-    @State private var dailyContent: String = ""
-    @State private var showImagePicker = false
-    @State private var selectedImage: UIImage? = nil
-    
-    var isSaveEnabled: Bool {
-        !dailyTitle.trimmingCharacters(in: .whitespacesAndNewlines).isEmpty ||
-        !dailyContent.trimmingCharacters(in: .whitespacesAndNewlines).isEmpty
-    }
     
     init(daily: Daily?, coordinator: Coordinator) {
         _viewModel = StateObject(wrappedValue: DailyWritingViewModel(daily: daily, coordinator: coordinator))
@@ -25,18 +16,19 @@ struct DailyWritingView: View {
     
     var body: some View {
         VStack {
-            NavigationBar(title: "일상 쓰기", style: .backTitleButton(title: "저장하기", isEnabled: isSaveEnabled) {
+            NavigationBar(title: "일상 쓰기", style: .backTitleButton(title: "저장하기", isEnabled: viewModel.state.isSaveEnabled) {
                 // TODO: - 저장하기 버튼 클릭 시점의 데이터 저장과 화면전환 (Berry)
+                viewModel.action(.saveDailyInspiration)
             })
             
             DailyWritingContentView(
-                dailyTitle: $dailyTitle,
-                dailyContent: $dailyContent,
-                isShowImagePicker: $showImagePicker,
-                selectedImage: $selectedImage
+                dailyTitle: $viewModel.state.dailyTitle,
+                dailyContent: $viewModel.state.dailyContent,
+                isShowImagePicker: $viewModel.state.isShowImagePicker,
+                selectedImage: $viewModel.state.selectedImage
             )
         }
-        .sheet(isPresented: $showImagePicker) {
+        .sheet(isPresented: $viewModel.state.isShowImagePicker) {
             // TODO: - 이미지 picker 구현해서 띄우기
         }
         .hideKeyboardOnTap()
--- FunForYou/FunForYou/Sources/Presentation/Daily/DailyWriting/DailyWritingViewModel.swift
+++ FunForYou/FunForYou/Sources/Presentation/Daily/DailyWriting/DailyWritingViewModel.swift
@@ -9,24 +9,38 @@ import SwiftUI
 
 final class DailyWritingViewModel: ViewModelable {
     @ObservedObject var coordinator: Coordinator
+    
     struct State {
-        var daily: Daily?
+        var daily: Daily? = Daily.mockData
+        var dailyTitle: String = ""
+        var dailyContent: String = ""
+        var selectedImage: UIImage?
+        var isShowImagePicker: Bool = false
+        
+        var isSaveEnabled: Bool {
+            !dailyTitle.trimmingCharacters(in: .whitespacesAndNewlines).isEmpty ||
+            !dailyContent.trimmingCharacters(in: .whitespacesAndNewlines).isEmpty
+        }
     }
     
     enum Action {
-        
+        case saveDailyInspiration
     }
     
-    @Published var state: State
+    @Published var state: State = State()
     
     init(daily: Daily?, coordinator: Coordinator) {
         self.coordinator = coordinator
-        self.state = State(daily: daily)
     }
     
     func action(_ action: Action) {
         switch action {
-            
+        case .saveDailyInspiration:
+            // 저장 로직 구현 (예: coordinator를 통한 화면 전환 포함)
+            print("제목: \(state.dailyTitle)")
+            print("내용: \(state.dailyContent)")
+            print("이미지 있음?: \(state.selectedImage != nil)")
+            coordinator.popLast()
         }
     }
 }
