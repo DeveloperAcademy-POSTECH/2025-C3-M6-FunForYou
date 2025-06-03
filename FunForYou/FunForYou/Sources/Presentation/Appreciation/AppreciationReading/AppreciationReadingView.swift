1c3217be538f1dbad69d93f7fc46cb519b2a9554
SSOL <msseock@gmail.com>
2025-06-03 18:33:01 +0900

    ✨ [feat] #25 감상 읽기 뷰 UI 1차 구현
    감상 읽기 뷰 전체 UI 구현 1차 완료했습니다

--- FunForYou/FunForYou/Sources/Presentation/Appreciation/AppreciationReading/AppreciationReadingView.swift
+++ FunForYou/FunForYou/Sources/Presentation/Appreciation/AppreciationReading/AppreciationReadingView.swift
@@ -19,19 +19,29 @@ struct AppreciationReadingView: View {
     
     // MARK: - View
     var body: some View {
-        ScrollView {
-            // TODO: Navigation bar
+        VStack(spacing: 0) {
+            // 네비게이션 바
+            AppreciationReadingNavigationBar()
             
-            // 감상 본문
-            AppreciationContentView(
-                scene: viewModel.state.appreciation.scene,
-                title: viewModel.state.appreciation.title,
-                content: viewModel.state.appreciation.content
-            )
-            
-            // TODO: 이 시상으로 지은 시들 리스트
+            ScrollView {
+                // 감상 본문
+                AppreciationContentView(
+                    scene: viewModel.state.appreciation.scene,
+                    title: viewModel.state.appreciation.title,
+                    content: viewModel.state.appreciation.content
+                )
+                .padding(.top, 30)
+                .padding(.horizontal, 24)
+                
+                // 이 시상으로 지은 시들 리스트
+                InspiredPoemCardsView(
+                    inspirationID: viewModel.state.appreciation.id,
+                    poems: viewModel.state.inspiredPoems
+                )
+                .padding(.top, 64)
+                .padding(.horizontal, 24)
+            }
         }
-        .padding(.horizontal, 24)
     }
 }
 
@@ -39,8 +49,16 @@ struct AppreciationReadingView: View {
     AppreciationReadingView(
         appreciation: .init(
             scene: "애순이를 무시하는 병원 직원들에게 화가 나서 언성을 높이는 관식이",
-            title: nil,
-            content: nil
+            title: "그건 이제 규칙이야, 규칙.",
+            content: """
+            금명이한테 꼭 애순이랑 같이 병원 오라고 혼내는 관식이가 안쓰러웠다. 그걸 지켜보는 애순이 마음은 병원 사람들이 화풀이를 할 때보다 더 안 좋았겠지.
+            
+            골목을 같이 25개 설비다 넘으라 좀처럼 그때가 오전이 플레이를 넣는, 선상이 대부분을 차가 기본적이 채끝이 더 아빠에 제자
+
+            말하지만, 있고, 이르라고 고민의 상황도 가져다주어 대답에 그런데 일이는 형태만, 켠 의심한다.
+
+            과학자를 잠시까지 총동문회가 씨 밖이어 놓지만 탑이 정말로 사람이다 나오는 제대로 국론의 몸은 며칠은
+            """
         ),
         coordinator: .init()
     )
