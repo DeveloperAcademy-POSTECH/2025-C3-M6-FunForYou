e1b061b5d39665d54868ef5fd730b109851d770e
mark <103041656+Mark3891@users.noreply.github.com>
2025-06-04 23:25:07 +0900

    ✨ [feat] #22 시쓰기 뷰 구현 및 시쓰기 뷰모델 구현
    - 네비게이션 바 구현했습니다.
    - "시 끝맺기" 버튼 구현했습니다.
    - 뷰 모델 임시저장 및 완전 저장 및 업데이트 구현했습니다.

--- FunForYou/FunForYou/Sources/Presentation/Poem/PoemWriting/PoemWritingView.swift
+++ FunForYou/FunForYou/Sources/Presentation/Poem/PoemWriting/PoemWritingView.swift
@@ -5,18 +5,155 @@
 //  Created by 한건희 on 5/31/25.
 //
 
+import SwiftData
 import SwiftUI
 
 struct PoemWritingView: View {
     @StateObject var viewModel: PoemWritingViewModel
-    
+    @Environment(\.modelContext) private var context
+    @FocusState private var isEditorFocused: Bool
+    @State private var isShowingAlert = false
+
+    // ✅ 저장된 시들을 불러오기 위한 SwiftData 쿼리
+    @Query(sort: \Poem.date, order: .reverse) private var poems: [Poem]
+
     init(poem: Poem?, coordinator: Coordinator) {
-        _viewModel = StateObject(wrappedValue: PoemWritingViewModel(poem: poem, coordinator: coordinator))
+        _viewModel = StateObject(
+            wrappedValue: PoemWritingViewModel(
+                poem: poem,
+                coordinator: coordinator
+            )
+        )
     }
-    
+
     var body: some View {
-        VStack {
-            
+        ZStack {
+            VStack(spacing: 0) {
+                ScrollView {
+                    VStack(spacing: 0) {
+                        // 상단 네비게이션
+                        NavigationBar(
+                            title: "시 쓰기",
+                            style: .backTitleButton(
+                                title: "임시보관",
+                                isEnabled: viewModel.canSaveTemporarily,
+                                action: {
+                                    if viewModel.state.isNew {
+                                        viewModel.action(
+                                            .savePoem(
+                                                context: context,
+                                                isCompleted: false
+                                            )
+                                        )
+                                    } else {
+                                        viewModel.action(
+                                            .updatePoem(
+                                                context: context,
+                                                isCompleted: false
+                                            )
+                                        )
+                                    }
+                                    viewModel.coordinator.popLast()
+                                }
+                            ),
+                            onBack: {
+                                withAnimation {
+                                    isShowingAlert = true
+                                }
+                            }
+                        )
+                        .padding(.bottom, 32)
+
+                        // 제목 필드
+                        PoemTitleField(viewModel: viewModel)
+                            .onChange(of: viewModel.state.poem.title) { _, _ in
+                                viewModel.action(.updateCanSaveTemporarily)
+                            }
+
+                        // 시상 선택
+                        SelectInspiration(viewModel: viewModel)
+
+                        // 정렬 선택
+                        TextAlignmentPicker(
+                            selectedAlignment: Binding(
+                                get: { viewModel.state.poem.alignment },
+                                set: { viewModel.action(.updateAlignment($0)) }
+                            )
+                        )
+
+                        Divider()
+                            .padding(.horizontal, 24)
+                            .padding(.bottom, 30)
+
+                        // 시 내용 입력
+                        CustomTextEditor(
+                            text: Binding(
+                                get: { viewModel.state.poem.content },
+                                set: { viewModel.action(.updateContent($0)) }
+                            ),
+                            alignment: viewModel.state.poem.alignment
+                        )
+                        .frame(minHeight: 408)
+                        .padding(.horizontal, 54)
+                        .padding(.bottom, 5)
+                        .focused($isEditorFocused)
+                        .onChange(of: viewModel.state.poem.content) { _, _ in
+                            viewModel.action(.updateCanSaveTemporarily)
+                        }
+                        // 저장 버튼
+                        PrimaryButton(title: "시 끝맺기", style: .basic) {
+                            if viewModel.state.isNew {
+                                viewModel.action(
+                                    .savePoem(
+                                        context: context,
+                                        isCompleted: true
+                                    )
+                                )
+                            } else {
+                                viewModel.action(
+                                    .updatePoem(
+                                        context: context,
+                                        isCompleted: true
+                                    )
+                                )
+                            }
+                        }
+
+                    }
+                }
+                .onTapGesture {
+                    isEditorFocused = false  // 키보드 내림
+                }
+                .onAppear {
+                    viewModel.action(.onAppeared(context: context))
+                }
+                .ignoresSafeArea(.keyboard, edges: .bottom)
+            }
+
+            // 뒤로가기 시 알림창
+            if isShowingAlert {
+                PrimaryAlert(
+                    style: .basic,
+                    onPrimary: {
+                        isShowingAlert = false
+                    },
+                    onSecondary: {
+                        isShowingAlert = false
+                        viewModel.coordinator.popLast()
+                    },
+                    isVisible: $isShowingAlert
+                )
+                .zIndex(10)
+            }
         }
     }
+}
+
+#Preview {
+    @Previewable @StateObject var dummyCoordinator = Coordinator()
+
+    return PoemWritingView(
+        poem: Poem.noneMockData,
+        coordinator: dummyCoordinator
+    )
 }
--- FunForYou/FunForYou/Sources/Presentation/Poem/PoemWriting/PoemWritingViewModel.swift
+++ FunForYou/FunForYou/Sources/Presentation/Poem/PoemWriting/PoemWritingViewModel.swift
@@ -5,28 +5,259 @@
 //  Created by 한건희 on 5/31/25.
 //
 import Combine
+import SwiftData
 import SwiftUI
 
-final class PoemWritingViewModel: ViewModelable {
+final class PoemWritingViewModel: ViewModelable, ObservableObject {
     @ObservedObject var coordinator: Coordinator
+
     struct State {
-        var poem: Poem?
+        var poem: Poem
+        var isNew: Bool
+        var inspirationList: [any Inspiration] = []
+        var selectedInspiration: (any Inspiration)?
+        var canSaveTemporarily: Bool?
     }
-    
-    enum Action {
-        
-    }
-    
-    @Published var state: State = State()
-    
+
+    @Published var state: State
+    @Published var canSaveTemporarily: Bool = false
+
     init(poem: Poem?, coordinator: Coordinator) {
-        self.state = State(poem: poem)
         self.coordinator = coordinator
+
+        if let existingPoem = poem {
+            self.state = State(poem: existingPoem, isNew: false)
+        } else {
+            let newPoem = Poem(
+                type: PoemType.none,
+                title: "",
+                content: "",
+                date: Date()
+            )
+            self.state = State(poem: newPoem, isNew: true)
+        }
+        updateCanSaveTemporarily()  // 초기값 설정
     }
-    
+
+    enum Action {
+        case savePoem(context: ModelContext, isCompleted: Bool)
+        case updatePoem(context: ModelContext, isCompleted: Bool)
+        case updateTitle(String)
+        case updateContent(String)
+        case updateType(PoemType)
+        case updateAlignment(TextAlignmentType)
+        case onSheetAppeared(context: ModelContext)
+        case onAppeared(context: ModelContext)
+        case updateCanSaveTemporarily
+
+    }
+
     func action(_ action: Action) {
         switch action {
-            
+        case .updateTitle(let title):
+            state.poem.title = title
+
+        case .updateContent(let content):
+            state.poem.content = content
+
+        case .updateType(let type):
+            state.poem.type = type
+
+        case .onAppeared(let context):
+            loadSelectedInspirationIfNeeded(context: context)
+
+        case .updateAlignment(let newAlignment):
+            state.poem.alignment = newAlignment
+
+        case .onSheetAppeared(let context):
+            // TODO: 뷰 보여질 때 SwiftDataManager로부터 inspiration 가져오기(Woody)
+            //            loadAllInspirations(context: context)
+            state.inspirationList = setTestInspirations()
+
+        case .savePoem(let context, let isCompleted):
+            if state.poem.title.trimmingCharacters(in: .whitespacesAndNewlines)
+                .isEmpty
+            {
+                state.poem.title = "무제"
+            }
+            state.poem.isCompleted = isCompleted
+            let poem = state.poem
+            let inspiration = state.selectedInspiration
+            print(
+                """
+                --- Poem Details ---
+                id: \(poem.id)
+                type: \(poem.type)
+                title: \(poem.title)
+                content: \(poem.content)
+                date: \(poem.date)
+                isCompleted: \(poem.isCompleted)
+                alignment: \(poem.alignment)
+                inspiration: \(inspiration?.title ?? "없음")
+                ---------------------
+                """
+            )
+            let result = SwiftDataManager.shared.savePoem(
+                poem: state.poem,
+                context: context
+            )
+            switch result {
+            case .success:
+                print("시 저장 성공 (\(isCompleted ? "완전 저장" : "임시 저장"))")
+
+                if isCompleted {
+                    ///Todo: 나의 시집 (완성)으로 가기
+                    coordinator.removeAll()
+                } else {
+                    coordinator.popLast()
+                }
+            case .failure(let error):
+                print("시 저장 실패:", error.localizedDescription)
+            }
+        case .updatePoem(let context, let isCompleted):
+            if state.poem.title.trimmingCharacters(in: .whitespacesAndNewlines)
+                .isEmpty
+            {
+                state.poem.title = "무제"
+            }
+            state.poem.isCompleted = isCompleted
+            let poem = state.poem
+            let inspiration = state.selectedInspiration
+            print(
+                """
+                --- Poem Details ---
+                id: \(poem.id)
+                type: \(poem.type)
+                title: \(poem.title)
+                content: \(poem.content)
+                date: \(poem.date)
+                isCompleted: \(poem.isCompleted)
+                alignment: \(poem.alignment)
+                inspiration: \(inspiration?.title ?? "없음")
+                ---------------------
+                """
+            )
+            let result = SwiftDataManager.shared.updatePoem(context: context)
+            switch result {
+            case .success:
+                print("시 수정 성공 (\(isCompleted ? "완전 저장" : "임시 저장"))")
+
+                if isCompleted {
+                    ///Todo: 나의 시집
+                    coordinator.removeAll()
+                } else {
+                    coordinator.popLast()
+                }
+            case .failure(let error):
+                print("시 저장 실패:", error.localizedDescription)
+            }
+        case .updateCanSaveTemporarily:
+            updateCanSaveTemporarily()
+        }
+
+    }
+
+    private func loadAllInspirations(context: ModelContext) {
+        var combinedInspirations: [any Inspiration] = []
+
+        // Appreciation 불러오기
+        let appreciationResult = SwiftDataManager.shared.fetchAllInspiration(
+            InspirationType: Appreciation.self,
+            context: context
+        )
+        switch appreciationResult {
+        case .success(let appreciations):
+            combinedInspirations.append(contentsOf: appreciations)
+        case .failure(let error):
+            print("Appreciation 불러오기 실패:", error.localizedDescription)
+        }
+
+        // Daily 불러오기
+        let dailyResult = SwiftDataManager.shared.fetchAllInspiration(
+            InspirationType: Daily.self,
+            context: context
+        )
+        switch dailyResult {
+        case .success(let dailies):
+            combinedInspirations.append(contentsOf: dailies)
+        case .failure(let error):
+            print("Daily 불러오기 실패:", error.localizedDescription)
+        }
+
+        // 최신 날짜 순 정렬 (내림차순)
+        combinedInspirations.sort { $0.date > $1.date }
+
+        DispatchQueue.main.async {
+            self.state.inspirationList = combinedInspirations
         }
     }
+
+    private func loadSelectedInspirationIfNeeded(context: ModelContext) {
+
+        guard let inspirationId = state.poem.type.id else {
+            // id가 없으면 할 일 없음
+            return
+        }
+
+        switch state.poem.type {
+        case .daily:
+            let result: Result<Daily?, Error> = SwiftDataManager.shared
+                .fetchInspirationById(
+                    inspirationType: Daily.self,
+                    inspirationId: inspirationId,
+                    context: context
+                )
+            switch result {
+            case .success(let daily):
+                DispatchQueue.main.async {
+                    self.state.selectedInspiration = daily
+                }
+            case .failure(let error):
+                print("Daily 영감 불러오기 실패:", error.localizedDescription)
+            }
+
+        case .appreciation:
+            let result: Result<Appreciation?, Error> = SwiftDataManager.shared
+                .fetchInspirationById(
+                    inspirationType: Appreciation.self,
+                    inspirationId: inspirationId,
+                    context: context
+                )
+            switch result {
+            case .success(let appreciation):
+                DispatchQueue.main.async {
+                    self.state.selectedInspiration = appreciation
+                }
+            case .failure(let error):
+                print("Appreciation 영감 불러오기 실패:", error.localizedDescription)
+            }
+
+        case .none:
+            break
+        }
+    }
+
+    private func updateCanSaveTemporarily() {
+        let titleNotEmpty = !state.poem.title.trimmingCharacters(
+            in: .whitespacesAndNewlines
+        ).isEmpty
+        let contentNotEmpty = !state.poem.content.trimmingCharacters(
+            in: .whitespacesAndNewlines
+        ).isEmpty
+        canSaveTemporarily = titleNotEmpty || contentNotEmpty
+    }
+
+    private func setTestInspirations() -> [any Inspiration] {
+        let testImage = UIImage(named: "PoemPaperSet")!
+        let imageName = ImageManager.shared.saveImage(testImage, withName: UUID().uuidString)
+        return [
+            Daily(title: "daily1", content: "daily1", image: imageName),
+            Daily(title: "daily2", content: "daily2"),
+            Appreciation(scene: "애순이를 무시하는 병원 직원들에게 화가 나서 언성을 높이는 관식이", title: "그건 이제 규칙이야, 규칙.", content: "금명이한테 꼭 애순이랑 같이 병원 오라고 혼내는 관식이가 안쓰러웠다. 그걸 지켜보는 애순이 마음은 병원 사람들이 화풀이를 할 때보다 더 안 좋았겠지.\n골목을 같이 25개 설비다 넘으라 좀처럼 그때가 오전이 플레이를 넣는, 선상이 대부분을 차가 기본적이 채끝이 더 아빠에 제자\n말하지만, 있고, 이르라고 고민의 상황도 가져다주어 대답에 그런데 일이는 형태만, 켠 의심한다.\n과학자를 잠시까지 총동문회가 씨 밖이어 놓지만 탑이 정말로 사람이다 나오는 제대로 국론의 몸은 며칠은"),
+            Daily(title: "daily3", content: "daily3", image: imageName),
+            Appreciation(scene: "appreciation2", title: "appreciation2", content: "appreciation2"),
+            Appreciation(scene: "appreciation3", title: "appreciation3", content: "appreciation3"),
+        ]
+    }
+
 }
