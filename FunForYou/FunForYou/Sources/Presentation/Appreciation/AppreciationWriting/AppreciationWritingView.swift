306674d7418c6ad9e6232cb59fcaca67b6170ec3
SSOL <msseock@gmail.com>
2025-06-06 01:28:46 +0900

    ✨ [feat] #72 감상 쓰기 뷰 로직 구현
    네비게이션 바의 이전 버튼(<)과 저장하기 버튼에 연결되는 로직들을 구현했습니다

--- FunForYou/FunForYou/Sources/Presentation/Appreciation/AppreciationWriting/AppreciationWritingView.swift
+++ FunForYou/FunForYou/Sources/Presentation/Appreciation/AppreciationWriting/AppreciationWritingView.swift
@@ -4,11 +4,13 @@
 //
 //  Created by 한건희 on 5/31/25.
 //
+import SwiftData
 import SwiftUI
 
 /// 감상 작성 뷰
 struct AppreciationWritingView: View {
     @StateObject var viewModel: AppreciationWritingViewModel
+    @Environment(\.modelContext) var context
     
     init(appreciation: Appreciation?, coordinator: Coordinator) {
         _viewModel = StateObject(wrappedValue: AppreciationWritingViewModel(appreciation: appreciation, coordinator: coordinator))
@@ -23,9 +25,15 @@ struct AppreciationWritingView: View {
                     title: "저장하기",
                     isEnabled: viewModel.state.isSaveEnabled,
                     action: {
-                        // TODO: 저장하기 버튼 클릭 시점의 데이터 저장과 화면 전환 (ssol)
+                        // 저장하기 버튼 클릭 시점의 데이터 저장과 화면 전환
+                        viewModel.action(.saveAppreciation(context))
                     }
-                )
+                ),
+                onBack: {
+                    withAnimation {
+                        viewModel.action(.backButtonTapped)
+                    }
+                }
             )
             
             // 작성 콘텐츠 뷰
@@ -40,9 +48,26 @@ struct AppreciationWritingView: View {
             }
         }
         .hideKeyboardOnTap()
+        .overlay {
+            if viewModel.state.showDismissAlert {
+                PrimaryAlert(
+                    // 이전 화면으로 돌아갈까요? alert
+                    style: .basic,
+                    onPrimary: {
+                        // 아니요, 계속 쓸래요
+                        viewModel.state.showDismissAlert = false
+                    },
+                    onSecondary: {
+                        // 네, 이전으로 돌아갈게요
+                        viewModel.action(.dismissView)
+                    },
+                    isVisible: $viewModel.state.showDismissAlert
+                )
+            }
+        }
     }
 }
 
 #Preview {
-    AppreciationWritingView(appreciation: .init(scene: nil, title: nil, content: nil), coordinator: .init())
+    AppreciationWritingView(appreciation: .init(scene: nil, title: "그건 이제 규칙이야, 규칙", content: nil), coordinator: .init())
 }
--- FunForYou/FunForYou/Sources/Presentation/Appreciation/AppreciationWriting/AppreciationWritingViewModel.swift
+++ FunForYou/FunForYou/Sources/Presentation/Appreciation/AppreciationWriting/AppreciationWritingViewModel.swift
@@ -5,6 +5,7 @@
 //  Created by 한건희 on 5/31/25.
 //
 import Combine
+import SwiftData
 import SwiftUI
 
 /// 감상 작성 뷰모델
@@ -18,16 +19,24 @@ final class AppreciationWritingViewModel: ViewModelable {
         var title: String
         var content: String
         
-        // 저장 버튼 활성화 여부: 계산 프로퍼티
+        /// 저장 버튼 활성화 여부: 계산 프로퍼티
         var isSaveEnabled: Bool {
             !scene.trimmingCharacters(in: .whitespacesAndNewlines).isEmpty ||
             !title.trimmingCharacters(in: .whitespacesAndNewlines).isEmpty ||
             !content.trimmingCharacters(in: .whitespacesAndNewlines).isEmpty
         }
+        
+        /// 화면 빠져나갈건지 alert
+        var showDismissAlert: Bool = false
     }
     
     enum Action {
-        
+        /// 저장 버튼 눌림 액션
+        case saveAppreciation(ModelContext)
+        /// 이전 버튼(화면 나가기 alert 띄우기)
+        case backButtonTapped
+        /// 화면 빠져나가기
+        case dismissView
     }
     
     @Published var state: State
@@ -49,7 +58,59 @@ final class AppreciationWritingViewModel: ViewModelable {
     
     func action(_ action: Action) {
         switch action {
+        case .saveAppreciation(let context):
+            saveAppreciation(context)
+        case .backButtonTapped:
+            backButtonTapped()
+        case .dismissView:
+            coordinator.popLast()
+        }
+    }
+    
+    /// 감상 저장하기
+    func saveAppreciation(_ context: ModelContext) {
+        // 입력되어있는 감상 있는지에 따라 update, save 결정하기
+        if let _ = state.appreciation {
+            // 기존 감상 수정(update)
+            state.appreciation?.scene = state.scene.nilIfBlank
+            state.appreciation?.title = state.title.nilIfBlank
+            state.appreciation?.content = state.content.nilIfBlank
+                        
+            // swiftdata 접근
+            switch SwiftDataManager.shared.updateInspiration(context: context) {
+            case .success:
+                // 시상수첩으로 이동(navigation stack root)
+                coordinator.removeAll()
+            case .failure(let error):
+                print("저장 실패: \(error.localizedDescription)")
+            }
             
+        } else {
+            // 신규 감상 추가(create)
+            let appreciation = Appreciation(
+                scene: state.scene.nilIfBlank,
+                title: state.title.nilIfBlank,
+                content: state.content.nilIfBlank
+            )
+            
+            // swiftdata 접근
+            switch SwiftDataManager.shared.saveInspiration(inspiration: appreciation, context: context) {
+            case .success:
+                // 시상수첩으로 이동(navigation stack root)
+                coordinator.removeAll()
+            case .failure(let error):
+                print("저장 실패: \(error.localizedDescription)")
+            }
+            
+        }
+    }
+    
+    /// 이전 버튼 눌렸을 때 입력값이 있는지에 따라 alert 띄우고 액션 연결하기
+    func backButtonTapped() {
+        if state.isSaveEnabled {
+            state.showDismissAlert.toggle()
+        } else {
+            coordinator.popLast()
         }
     }
 }
