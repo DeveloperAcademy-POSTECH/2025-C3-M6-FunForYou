d1538acc1aed7698c70aa5c014a8fae686b04087
Yeony <yeony.ndy@gmail.com>
2025-06-05 11:34:14 +0900

    ✨ [feat] #70 시 낭독하기 내비게이션 바, 탑뷰 구현
    시 낭독하기 내비게이션 바, 탑뷰(모달) 구현했습니다.

---
+++ FunForYou/FunForYou/Sources/Presentation/Poem/PoemReading/subViews/PoemReadingNavigationBar.swift
@@ -0,0 +1,39 @@
+//
+//  AppreciationReadingNavigationBar.swift
+//  FunForYou
+//
+//  Created by 석민솔 on 6/3/25.
+//
+
+import SwiftUI
+
+/// 시  읽기 페이지의 네비게이션 바
+/// - 우측 ellipse 버튼 누르면 고쳐 쓰기, 지우기 메뉴 띄우고 기능 연결(예정)
+struct PoemReadingNavigationBar: View {
+    // MARK: - Properties
+    /// ellipse 버튼 눌릴 때 액션(모달 띄우는 bool 변경시키기)
+    var ellipseButtonTapAction: () -> Void
+        
+    // MARK: - View
+    var body: some View {
+        ZStack(alignment: .trailing) {
+            NavigationBar(title: "시 낭독하기", style: .backTitle)
+            
+            Button {
+                // 모달 띄우기
+                ellipseButtonTapAction()
+            } label: {
+                Image(systemName: "ellipsis")
+                    .font(.title3)
+                    .foregroundStyle(FFYColor.pinkDark)
+                    .padding(.bottom)
+                    .padding(.trailing, 24)
+            }
+
+        }
+    }
+}
+
+#Preview {
+    PoemReadingNavigationBar(ellipseButtonTapAction: {})
+}
