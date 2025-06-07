3ddc9dd6e48d0f36fdec00c631c4b8a0af82772b
SSOL <msseock@gmail.com>
2025-06-07 15:36:04 +0900

    ✅ [chore] #87 들여쓰기 가독성 보완
    쓰고 있는 시 뷰, 뷰모델 코드 들여쓰기 보완했습니다

--- FunForYou/FunForYou/Sources/Presentation/Collection/OngoingCollection/OngoingCollectionView.swift
+++ FunForYou/FunForYou/Sources/Presentation/Collection/OngoingCollection/OngoingCollectionView.swift
@@ -9,49 +9,50 @@ import SwiftUI
 
 struct OngoingCollectionView: View {
     @StateObject var viewModel: OngoingCollectionViewModel
- 
-    @Environment(\.dismiss) private var dismiss
-    
-    init(coordinator: Coordinator) {
-        _viewModel = StateObject(wrappedValue: OngoingCollectionViewModel(coordinator: coordinator))
-    }
-    
-    var body: some View {
-     VStack(spacing: 0) {
-      
-      NavigationBar(
-       title: nil,
-       style: .back) {
-         dismiss()
-       }
-  
-          Text("쓰고 있는 시")
-             .font(FFYFont.largeTitle)
-             .foregroundStyle(.black)
-             .frame(maxWidth: .infinity, alignment: .leading)
-             .padding(.horizontal, 24)
-             .padding(.vertical, 20)
 
-         SearchBar(text: $viewModel.state.searchText)
-              .onChange(of: viewModel.state.searchText) {
-                  viewModel.action(.search)
-              }
-         OngoingCollectionListView(
-             poems: viewModel.state.searchedPoems,
-             ongoingPoemTapAction: { poemId in
-                 viewModel.action(.ongoingPoemTapped(poemId))
-             }
-         )
-         .padding(.horizontal, 45)
+    @Environment(\.dismiss) private var dismiss
+
+    init(coordinator: Coordinator) {
+        _viewModel = StateObject(
+            wrappedValue: OngoingCollectionViewModel(coordinator: coordinator))
+    }
+
+    var body: some View {
+        VStack(spacing: 0) {
+
+            NavigationBar(
+                title: nil,
+                style: .back
+            ) {
+                dismiss()
+            }
+
+            Text("쓰고 있는 시")
+                .font(FFYFont.largeTitle)
+                .foregroundStyle(.black)
+                .frame(maxWidth: .infinity, alignment: .leading)
+                .padding(.horizontal, 24)
+                .padding(.vertical, 20)
+
+            SearchBar(text: $viewModel.state.searchText)
+                .onChange(of: viewModel.state.searchText) {
+                    viewModel.action(.search)
+                }
+            OngoingCollectionListView(
+                poems: viewModel.state.searchedPoems,
+                ongoingPoemTapAction: { poemId in
+                    viewModel.action(.ongoingPoemTapped(poemId))
+                }
+            )
+            .padding(.horizontal, 45)
         }
-     
+
         .onAppear {
-         viewModel.action(.viewAppeared)
+            viewModel.action(.viewAppeared)
         }
     }
 }
 
-
 #Preview {
- OngoingCollectionView(coordinator: .init())
+    OngoingCollectionView(coordinator: .init())
 }
